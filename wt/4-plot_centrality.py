a0f9b2466e777947cb458cc6bde9ab9cc260f708
mahdi-robbani <mahdi.robbani@gmail.com>
2021-03-19 15:53:12 +0100

    Added network plotting

--- wt/4-plot_centrality.py
+++ wt/4-plot_centrality.py
@@ -5,10 +5,46 @@ import seaborn as sns
 import itertools
 from sklearn.linear_model import LinearRegression
 from scipy.stats import pearsonr
+import networkx as nx
+import MDAnalysis as mda
+
+def build_graph(fname, pdb = None):
+    """Build a graph from the provided matrix"""
+
+    try:
+        adj_matrix = np.loadtxt(fname)
+    except:
+        errstr = f"Could not load file {fname} or wrong file format."
+        raise ValueError(errstr)
+    # if the user provided a reference structure
+    if pdb is not None:
+        try:
+            # generate a Universe object from the PDB file
+            u = mda.Universe(pdb)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"PDB not found: {pdb}")
+        except:
+            raise Exception(f"Could not parse pdb file: {pdb}")
+        # generate identifiers for the nodes of the graph
+        identifiers = [f"{r.segment.segid}{r.resnum}" for r in u.residues]
+    # if the user did not provide a reference structure
+    else:
+        # generate automatic identifiers going from 1 to the
+        # total number of residues considered
+        identifiers = [f"_{i}" for i in range(1, adj_matrix.shape[0]+1)]
+    
+    # generate a graph from the data loaded
+    G = nx.Graph(adj_matrix)
+    # set the names of the graph nodes (in place)
+    node_names = dict(zip(range(adj_matrix.shape[0]), identifiers))
+    nx.relabel_nodes(G, mapping = node_names, copy = False)
+    # return the idenfiers and the graph
+    return identifiers, G
 
 # set directory
 file_path = "centrality/flow.txt"
 out_dir = "plots/"
+PLOT = False
 
 def replace_dict(names):
     new_names = []
@@ -73,10 +109,10 @@ basic = ['degree', 'betweenness', 'closeness', 'eigenvector']
 plot_centrality_vs_residues(data, basic, sds, "basic")
 # plot only flow centralities
 flow = ['current_betweenness_c0', 'current_closeness_c0', 'current_betweenness_c1', 'current_closeness_c1', 'current_betweenness_c2', 'current_closeness_c2']
-plot_centrality_vs_residues(data, flow, sds, "flow")
-
-#plot heatmap
-heatmap(data, new_colnames, "flow") #xticklabels
+if PLOT:
+    plot_centrality_vs_residues(data, flow, sds, "flow")
+    #plot heatmap
+    heatmap(data, new_colnames, "flow") #xticklabels
 
 #combine connected component cols
 
@@ -93,11 +129,60 @@ del_cols = ["current_betweenness", "current_closeness"] #maybe add communicabili
 data_combined = combine_cols(data, del_cols)
 combined_colnames = list(data_combined.columns)[2:]
 #plot 2
-heatmap(data_combined, combined_colnames, "flow_combined")
-plot_centrality_vs_residues(data_combined, combined_colnames, sds, "flow_combined", (10, 20))
+if PLOT:
+    heatmap(data_combined, combined_colnames, "flow_combined")
+    plot_centrality_vs_residues(data_combined, combined_colnames, sds, "flow_combined", (10, 20))
+
+def remove_isolates(G):
+    """Takes in a graph and returns a new graph where all the nodes with 
+    zero edges have been removed.
+    """
+
+    # create duplicate graph so the original is unaffected
+    H = G.copy()
+    # isolates are nodes with zero edges
+    isolates = nx.isolates(G)
+    # remove from duplicate graph
+    H.remove_nodes_from(list(isolates))
+    return H
+
+def keep_largest_comonent(G):
+    H = G.copy()
+    components = nx.algorithms.components.connected_components(G)
+    remove = sorted(components, key = lambda x: len(x))[:-1]
+    for nodes in remove:
+        H.remove_nodes_from(nodes)
+    return H
+
+def get_reduced_df(df, H):
+    pos = nx.spring_layout(H, k = 0.5)
+    node_list = H.nodes()
+    reduced_df = df.loc[df['node'].isin(node_list)]
+    return reduced_df, pos
+
+
+def plot_graph(G, df, measure):
+    nodes = df['node']
+    lab = {node:node[1:] for node in nodes}
+    weights = df[measure]
+    ec = nx.draw_networkx_edges(G, pos)
+    nc = nx.draw_networkx_nodes(G, pos, nodelist = nodes, node_color = weights, cmap = plt.cm.hot, edgecolors='black')
+    plt.colorbar(nc)
+    nx.draw_networkx_labels(G, pos, labels = lab, font_size=9, font_color ='midnightblue')
+    plt.axis('off')
+    plt.title(f"{measure} centrality")
+    plt.savefig(f'{out_dir}{measure}_graph.png')
+    plt.clf()
 
 
 
+nodes, G = build_graph("hc_graph_filtered.dat", "model0_A.pdb")
+H = remove_isolates(G)
+C = keep_largest_comonent(G)
+data_largest_comp, pos = get_reduced_df(data, C)
+
+for col in basic:
+    plot_graph(C, data_largest_comp, col)
 
 
 # def plot_centrality_vs_residues(data, columns, sds, fname = ""):
