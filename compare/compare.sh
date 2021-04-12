aeb49a95013553ddc678b69e58777c505f868439
mahdi-robbani <mahdi.robbani@gmail.com>
2021-04-12 18:04:21 +0200

    ignored test folders

---
+++ compare/compare.sh
@@ -0,0 +1,7 @@
+python ../wt/4-plot_centrality.py \
+    -wt ../wt/centrality/flow.txt  \
+    -mt ../s99t/centrality/s99t.txt \
+    -d ../wt/hc_graph_filtered.dat \
+    -p ../wt/model0_A.pdb \
+    -dg \
+    -o diff_network
