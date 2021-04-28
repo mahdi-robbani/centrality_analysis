262d6a1bc2bf4d570bcfc4e620c4bcca23ab4b1a
mahdi-robbani <mahdi.robbani@gmail.com>
2021-04-28 14:08:04 +0200

    added option to change fie name and n_swaps

--- wt/random/get_random_cent.sh
+++ wt/random/get_random_cent.sh
@@ -2,13 +2,16 @@ graph_script=../9-random_graph.py
 cent_script=../../../pyinteraph2/pyinteraph/centrality_analysis.py
 psn=../hc_graph_filtered.dat
 pdb=../model0_A.pdb
+n_swaps=20
+out=x #CHANGE FILENAME
 
 
 
 for i in {1..100}
 do
     echo run $i
-    python $graph_script -i $psn -p $pdb -n 10 -o tmp
+    python $graph_script -i $psn -p $pdb -n $n_swaps -o tmp
     python $cent_script -i tmp.dat -r $pdb -c degree betweenness closeness eigenvector -o tmp
-    tail -n +2 tmp.txt >> agg.txt
+    tail -n +2 tmp.txt >> $out
 done
+
