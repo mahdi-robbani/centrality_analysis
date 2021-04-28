83d0f0878dee7b852294135e5d95849fcb6adc25
mahdi-robbani <mahdi.robbani@gmail.com>
2021-04-28 14:19:40 +0200

    added unfiltered calculation

--- s99t/3-centrality.sh
+++ s99t/3-centrality.sh
@@ -1,13 +1,17 @@
 dir=../../pyinteraph2/pyinteraph
 script=$dir/centrality_analysis.py
 inp=hc_graph_filtered.dat
+unf=hc_graph.dat
 pdb=model0.pdb 
 pdb_A=model0_A.pdb
 # remember to activate virtual environment
 
 #node centralities
 #python $script -i $inp -r $pdb_A -c all -o centrality/s99t -p
+python $script -i $unf -r $pdb_A -w -c all -o unfiltered/s99t_unf_w -p
+#python $script -i $inp -r $pdb_A -w -c all -o centrality/s99t_w -p
 
 #edge centralities
-python $script -i $inp -r $pdb_A -c edge_betweenness -o centrality/edge
+#python $script -i $inp -r $pdb_A -c edge_betweenness -o centrality/edge
+
 
