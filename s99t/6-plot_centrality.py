8b872e6b02c6e8911be8f586a0d215b44f1934ee
mahdi-robbani <mahdi.robbani@gmail.com>
2021-05-10 15:01:46 +0200

    copied from wt and changed raneg dictionary

--- s99t/6-plot_centrality.py
+++ s99t/6-plot_centrality.py
@@ -9,23 +9,40 @@ import pickle
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 from collections import Counter
 from pyinteraph import path_analysis as pa
+import adjustText
+import random
 
 WT_DICT = {"Degree": 0.030864,
-              "Betweenness": 0.144122,
+              "Betweenness": 0.201798,
               "Closeness": 0.067009,
               "Eigenvector": 0.519175}
 
-RANGE_DICT = {"Degree": 0.035,
-              "Betweenness": 0.15,
-              "Closeness": 0.10,
-              "Eigenvector": 0.55,
-              "CF_betweenness_1" : 0.75,
-              "CF_betweenness_2" : 0.75,
-              "CF_betweenness_3" : 0.75,
-              "CF_closeness_1" : 0.135,
-              "CF_closeness_2" : 0.135,
-              "CF_closeness_3" : 0.135,
+# RANGE_DICT = {"Degree": 0.035,
+#               "Betweenness": 0.20,
+#               "Closeness": 0.10,
+#               "Eigenvector": 0.55,
+#               "CF_betweenness_1" : 0.75,
+#               "CF_betweenness_2" : 0.75,
+#               "CF_betweenness_3" : 0.75,
+#               "CF_closeness_1" : 0.135,
+#               "CF_closeness_2" : 0.135,
+#               "CF_closeness_3" : 0.135,
+#               }
+
+CUTOFF_DICT = {"Degree": 5,
+              "Betweenness": 5,
+              "Closeness": 5,
+              "Eigenvector": 5,
+              "CF_betweenness_1" : 3,
+              "CF_betweenness_2" : 3,
+              "CF_betweenness_3" : 3,
+              "CF_betweenness_4" : 3,
+              "CF_betweenness_5" : 3,
+              "CF_closeness_1" : 3,
+              "CF_closeness_2" : 3,
+              "CF_closeness_3" : 3,
               }
+
 
 RES_DICT = {
     'ALA':  'A','ARG':	'R','ASN':	'N','ASP':	'D',
@@ -61,37 +78,60 @@ def load_sasa(file):
     data['Node'] = data['ID'].apply(lambda x: 'A' + str(x))
     return data
 
-def combine_cent_sasa(cent_file, sasa_file):
-    cent = load_centrality(cent_file)
-    # sasa = load_sasa(sasa_file)
-    # sasa = sasa.drop(columns = ['Name'])
-    # data = cent.merge(sasa, on = 'Node')
-    return cent
+def load_ddg(file):
+    data = pd.read_csv(file, sep = "\t")
+    data['Node'] = data['Node'].apply(lambda x: 'A' + str(x))
+    data = data.drop(["Res"], axis = 1)
+    return data
+
+def combine_cent_sasa(cent_file, sasa_file = False, ddg_file = False):
+    data = load_centrality(cent_file)
+    if sasa_file:
+        sasa = load_sasa(sasa_file)
+        sasa = sasa.drop(columns = ['Name'])
+        data = data.merge(sasa, on = 'Node')
+    if ddg_file:
+        ddg = load_ddg(ddg_file)
+        data = data.merge(ddg, on = 'Node')
+    return data
 
 # plotting functions
 
-def plot_centrality_vs_residues(data, columns, sds, fname, out_dir, share_y, max_range = None, size = (9, 7)):
+def plot_centrality_vs_residues(data, columns, top_n, fname, out_dir, share_y, max_range = None, size = (9, 7)):
+    active_site = ['A55','A60','A102','A113','A122','A126']
+    active_site = ['A6','A29','A63','A66','A66','A99']
     node_dict = {}
     nrows = len(columns)//2 if len(columns) % 2 == 0 else len(columns)//2 + 1
-    for sd in sds:
+    for n in top_n:
         fig, axs = plt.subplots(nrows, 2, figsize = size, sharex= True, sharey= share_y)
         for i, ax in enumerate(axs.flat):
             if i < len(columns):
-                ax.plot(data[columns[i]], label = columns[i])
+                ax.scatter(x = range(len(data[columns[i]])), y = data[columns[i]], edgecolors = 'black', alpha = 0.7)
                 ax.set_title(columns[i])
                 if max_range == "max":
                     ax.set_ylim(top = 1)
                 elif max_range == "range_dict":
                     ax.set_ylim(top = RANGE_DICT[columns[i]])
-                mean_std = [data[columns[i]].mean(), data[columns[i]].std()]
-                cutoff = mean_std[0] + sd*mean_std[1]
+                # GET CUTOFF
+                #mean_std = [data[columns[i]].mean(), data[columns[i]].std()]
+                #cutoff = mean_std[0] + sd*mean_std[1]
+                cutoff = sorted(data[columns[i]], reverse = True)[n:n+1][0] - 1e-06
+                #print(cutoff)
                 node_list = []
+                texts = []
+                max_val = data[columns[i]].max()
                 for j, val in enumerate(data[columns[i]]):
-                    if val > cutoff:
-                        ax.annotate(data['Residue'][j], (j, val))
-                        node_list.append(data['Residue'][j])
+                    if val > cutoff and val > 0:
+                        ax.annotate(data['Residue'][j], (j, val), alpha = 0.8)
+                        #node_list.append(data['Residue'][j])
+                        #texts.append(ax.text(j, val, data['Residue'][j]))
+                    if data['Node'][j] in active_site:
+                        #texts.append(ax.text(j, val, data['Residue'][j], color = "purple"))
+                        ax.annotate(data['Residue'][j], (j, val), color = "purple", alpha = 0.8)
+                adjustText.adjust_text(texts)
                 node_dict.update({columns[i] : node_list})
-                ax.hlines(y = cutoff, xmin = 0, xmax = len(data['Node']), linestyles = "dashed")
+                #plot horizontal line at cutoff
+                #ax.hlines(y = cutoff, xmin = 0, xmax = len(data['Node']), linestyles = "dashed")
                 if i % 2 == 0:
                     ax.set(ylabel='Centrality Value')
                 if i == (nrows*2)-2 or i == (nrows*2)-1:
@@ -101,53 +141,56 @@ def plot_centrality_vs_residues(data, columns, sds, fname, out_dir, share_y, max
             else:
                 ax.set_visible(False)
         fig.tight_layout()
-        plt.savefig(f"{out_dir}/centrality_{sd}_{fname}{w}.pdf")
+        plt.savefig(f"{out_dir}/centrality_{n}_{fname}.pdf")
         plt.clf()
     return node_dict
 
 def heatmap(data, colnames, fname, oudir):
-    cor = data[colnames].corr().round(2)
+    #ignore glycines
+    data = data.loc[data['Name'] != "GLY"]
+    new_colnames = [f"Mutant Count > {col.split('_')[1]}" if col.split('_')[0] == "Count" else col for col in colnames]
+    col_dict = {col : f"Mutant Count > {col.split('_')[1]}" for col in data.columns if col.split('_')[0] == "Count"}
+    data = data.rename(columns = col_dict)
+    cor = data[new_colnames].corr().round(2)
     plt.figure(figsize=(10,8))
     sns.heatmap(cor, cmap="RdBu_r", annot = True)
     plt.tight_layout()
-    plt.savefig(f"{out_dir}/correlation_{fname}{w}.pdf")
+    plt.savefig(f"{out_dir}/correlation_{fname}.pdf")
     plt.clf()
 
 
 # set directory and files
-w = True
-if w:
-    cent_file = "centrality/s99t_w.txt"
-    w = "_w"
-else:
-    cent_file = "centrality/s99t.txt"
-    w = ""
+cent_file = "centrality/s99t.txt"
 #sasa_file = "sasa.txt"
+ddg_dict = {"mean" : "../mutatex/mut_data/mean_per_pos_df.txt",
+            "median" : "../mutatex/mut_data/median_per_pos_df.txt",
+            "count_3" : "../mutatex/mut_data/Count_3_df.txt"}
+ddg_file = "../mutatex/mut_data/per_pos_df.txt"
 psn_file = "hc_graph_filtered.dat"
 pdb_file = "model0_A.pdb"
 out_dir = "plots/"
 
-
-
-
-
 # load and fix df
-data = combine_cent_sasa(cent_file, None)#sasa_file)
+# replace sasa_file/dgg_file with None if not present
+data = combine_cent_sasa(cent_file, False, ddg_file)
+# #ignore glycines
+# data = data.loc[data['Name'] != "GLY"]
+# data = data.reset_index()
 # get colnames
 basic_cols = ['Degree', 'Betweenness', 'Closeness', 'Eigenvector']
 cf_cols = [c for c in data.columns if "CF_" in c]
 cf_cols = sorted(cf_cols, key = lambda c : c.split('_')[2:])
+cf_cols = cf_cols[:6]
 
 #plot toggles
-plot = False
-CENT_B = True
+plot = True
+CENT_B = plot
 CENT_CF = plot
-CORR = plot
-NETWORK = True
-
+CORR = False
+NETWORK = plot
 
 def get_count(node_dict):
-    print(node_dict)
+    #print(node_dict)
     node_list = []
     for nodes in node_dict.values():
         node_list += nodes
@@ -156,41 +199,45 @@ def get_count(node_dict):
     return node_list
 
 # plot centrality vs residues
-
+# top n residues
 sds = [3]
 
 # plot basic
 if CENT_B:
     print("plotting centrality vs residues for basic cenralities")
     # plot custom plots (defined by range dict)
-    mut_nodes = plot_centrality_vs_residues(data, basic_cols, sds, "basic", out_dir, share_y = False, max_range = "range_dict")
-    print(get_count(mut_nodes))
-    plot_centrality_vs_residues(data, basic_cols, sds, "basic_f", out_dir, share_y = False)
+    wt_nodes = plot_centrality_vs_residues(data, basic_cols, sds, "basic_f", out_dir, share_y = False)
+    print(get_count(wt_nodes))
     # Plot absolute plots (max range [0-1])
-    plot_centrality_vs_residues(data, basic_cols, sds, "basic_m", out_dir, share_y = False, max_range = "max")
-
+    #plot_centrality_vs_residues(data, basic_cols, sds, "basic_m", out_dir, share_y = False, max_range = "max")
+    # #plot sasa and ddg values
+    # ddg_cols = ["SASA", "Mean DDG", "Median DDG", "Count_3"]
+    # plot_centrality_vs_residues(data, ddg_cols, sds, "ddg", out_dir, share_y = False)
 
 # plot only flow centralities
 if CENT_CF:
     print("plotting centrality vs residues for CF cenralities")
     # Plot relative plots (default range)
-    plot_centrality_vs_residues(data, cf_cols, sds, "cf", out_dir, share_y = False, max_range = "range_dict")
+    plot_centrality_vs_residues(data, cf_cols, sds, "cf", out_dir, share_y = False)
     # Plot absolute plots (max range [0-1])
     plot_centrality_vs_residues(data, cf_cols, sds, "cf_m", out_dir, share_y = False, max_range = "max")
-
-
-
 
 # plot heatmap
 if CORR:
     #plot heatmap
     print("plotting heatmap for all cenralities")
-    sasa_cols = basic_cols #+ cf_cols + ['SASA']
-    heatmap(data, sasa_cols, "sasa", out_dir)
+    cent_cols = basic_cols + cf_cols
+    heatmap(data, cent_cols, "noG", out_dir)
+    print("plotting heatmap for all sasa cenralities")
+    sasa_cols = basic_cols + ['SASA', 'Mean DDG', 'Median DDG', 'Count_3']
+    heatmap(data, sasa_cols, "ddg_noG", out_dir)
+    # print("plotting heatmap for all count cenralities")
+    # sasa_cols = basic_cols + ['SASA', 'Mean DDG', 'Median DDG'] + [f"Count_{i}" for i in range(0, 12)]
+    # heatmap(data, sasa_cols, "ddg_count_noG", out_dir)
 
 # save pos
-def save_pos(pos):
-    with open('pos.bin', 'wb') as f:
+def save_pos(pos, name = f"pos.bin"):
+    with open(name, 'wb') as f:
         pickle.dump(pos, f)
 
 # load pos
@@ -199,7 +246,6 @@ def load_pos(file):
         data = pickle.load(f)
     return data
 
-
 def get_graph_pos(psn, pdb):
     nodes, names, G = pa.build_graph(psn, pdb)
     weighted_edges = [(u, v, d["weight"]) for u, v, d in G.edges(data=True)]
@@ -207,12 +253,13 @@ def get_graph_pos(psn, pdb):
     # H.add_weighted_edges_from(weighted_edges)
     H = G
     # k = 0.5, iterations = 100
-    pos = nx.spring_layout(H, k = 0.5, seed = 1, iterations = 100)
-    #save_pos(pos)
-    pos = load_pos("../wt/pos.bin")
+    # pos = nx.spring_layout(H, k = 0.55, seed = 1, iterations = 100)
+    # save_pos(pos)
+    pos = load_pos("../wt/pos.bin") #CHANGE
+    #print(pos)
     return H, pos
 
-def plot_graph(G, pos, df, measure, out_dir, r_dict = True):
+def plot_graph(G, pos, df, measure, out_dir, ext, r_dict = False):
     vmax = WT_DICT[measure] if r_dict else None
     r = "" if r_dict else "_f"
     isolates = list(nx.isolates(G))
@@ -238,14 +285,14 @@ def plot_graph(G, pos, df, measure, out_dir, r_dict = True):
     plt.axis('off')
     #plt.tight_layout()
     plt.title(f"{measure} centrality")
-    plt.savefig(f'{out_dir}{measure}_graph{r}{w}.pdf')
+    plt.savefig(f'{out_dir}{measure}_graph{r}_{ext}.pdf')
     plt.clf()
 
 G, pos = get_graph_pos(psn_file, pdb_file)
 if NETWORK:
-    for measure in basic_cols:
+    for measure in basic_cols: #+ ['Mean DDG', 'Median DDG', 'Count_3']:
         print(f"Plotting network: {measure}")
-        plot_graph(G, pos, data, measure, out_dir, r_dict = False)
+        plot_graph(G, pos, data, measure, out_dir, "", r_dict = True)
         # if measure == "Betweenness":
         #     break
 
