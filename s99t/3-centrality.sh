934e68352cf311cbde8c36512079cb9b3bf33c2a
mahdi-robbani <mahdi.robbani@gmail.com>
2021-05-10 15:00:53 +0200

    centrality measures consider endpoints

--- s99t/3-centrality.sh
+++ s99t/3-centrality.sh
@@ -1,16 +1,11 @@
 dir=../../pyinteraph2/pyinteraph
 script=$dir/centrality_analysis.py
 inp=hc_graph_filtered.dat
-unf=hc_graph.dat
-pdb=model0.pdb 
 pdb_A=model0_A.pdb
 # remember to activate virtual environment
 
-#node centralities
-#python $script -i $inp -r $pdb_A -c all -o centrality/s99t -p
-python $script -i $unf -r $pdb_A -w -c all -o unfiltered/s99t_unf_w -p
-#python $script -i $inp -r $pdb_A -w -c all -o centrality/s99t_w -p
-
+#node centralities (use edge weights)
+python $script -i $inp -r $pdb_A -e -c all -o centrality/s99t -p
 #edge centralities
 #python $script -i $inp -r $pdb_A -c edge_betweenness -o centrality/edge
 
